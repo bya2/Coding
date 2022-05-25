3dca5939e817b1bc08a3fb7581e29551ab95640a
bya2 <byaa1972@gmail.com>
2022-05-25 18:29:20 +0900

    success: 프로그래머스/신규 아이디 추천

---
+++ src/programmers/신규 아이디 추천.mjs
@@ -0,0 +1,45 @@
+// 3 <= id <= 15
+// string(lowercase, number, -, _, .)
+// .는 처음과 끝 불가, 연속 사용 불가
+
+export const solution = (new_id) => {
+  new_id = new_id
+    .toLocaleLowerCase("en-US")
+    .replace(/[^-\_\.a-z0-9]/g, "")
+    .replace(/\.{2,}/g, ".");
+
+  new_id = new_id[0] === "." ? new_id.slice(1) : new_id[new_id.length - 1] === "." ? new_id.slice(0, -1) : new_id;
+  new_id = new_id ? new_id : "a";
+  new_id = new_id.length >= 16 ? new_id.slice(0, 15) : new_id;
+  new_id = new_id[new_id.length - 1] === "." ? new_id.slice(0, -1) : new_id;
+
+  if (new_id.length <= 2) {
+    const char = new_id[new_id.length - 1];
+    new_id += new Array(3 - new_id.length).fill(char).join("");
+  }
+
+  return new_id;
+};
+
+export const examples__arr = [
+  {
+    new_id: "...!@BaT#*..y.abcdefghijklm",
+    answer: "bat.y.abcdefghi",
+  },
+  {
+    new_id: "z-+.^.",
+    answer: "z--",
+  },
+  {
+    new_id: "=.=",
+    answer: "aaa",
+  },
+  {
+    new_id: "123_.def",
+    answer: "123_.def",
+  },
+  {
+    new_id: "abcdefghijklmn.p",
+    answer: "abcdefghijklmn",
+  },
+];
--- src/run.mjs
+++ src/run.mjs
@@ -1,5 +1,5 @@
 import runner from "./runner.mjs";
-import { solution, examples__arr } from "./programmers/신고 결과 받기.mjs";
+import { solution, examples__arr } from "./programmers/신규 아이디 추천.mjs";
 
 (function () {
   console.log("Solve:");
