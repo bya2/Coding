2445093104465424501a10e7738ad4ca864f6c2b
bya2 <byaa1972@gmail.com>
2022-05-29 00:15:34 +0900

    success: 프로그래머스/단어 변환 (BFS)

--- src/programmers/단어 변환.mjs
+++ src/programmers/단어 변환.mjs
@@ -8,27 +8,26 @@ export const solution = (begin, target, words) => {
   let steps = 0;
   const queue = [[begin, steps]];
   const visited = [];
+
   while (queue.length) {
-    let [visit_word, count] = queue.shift();
+    let [curr_word, step] = queue.shift();  
 
-    if (visit_word === target) {
-      return count;
-    }
+    if (curr_word === target) return step;
 
-    visited.push(visit_word);
+    visited.push(curr_word);
 
     for (const word of words) {
       if (visited.includes(word)) continue; // 이미 방문
 
       let not_eq = 0; // 하나의 단어만 변경해야함.
       for (let i = 0; i < word.length; ++i) {
-        if (word[i] !== visit_word[i]) {
+        if (word[i] !== curr_word[i]) {
           ++not_eq;
         }
       }
 
       if (not_eq === 1) {
-        queue.push([word, ++count]);
+        queue.push([word, step + 1]);
       }
     }
   }
