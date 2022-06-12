8ed62e9b28e23a7073abf2fd19501193806856c7
bya2 <byaa1972@gmail.com>
2022-06-12 21:51:40 +0900

    other: 프로그래머스/멀쩡한 사각형 (기울기, 일차방정식)

--- src/other.mjs
+++ src/other.mjs
@@ -1,5 +1,5 @@
 import runner from "./runner.mjs";
-import { other_solution, examples__arr } from "./programmers/음양 더하기.mjs";
+import { other_solution, examples__arr } from "./programmers/멀쩡한 사각형.mjs";
 
 (function () {
   console.log("Others:");
