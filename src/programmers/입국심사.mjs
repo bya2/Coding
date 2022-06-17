5aff4a9665ec2ae3d12f17828e50f7419ec929f1
bya2 <byaa1972@gmail.com>
2022-06-17 13:08:20 +0900

    success: 프로그래머스/입국심사 - 이분탐색

--- src/programmers/입국심사.mjs
+++ src/programmers/입국심사.mjs
@@ -1,5 +1,56 @@
 // 이분 탐색
 
+export const binarySearchAlgorithm = (_arr, _tg) => {
+  let low = 0;
+  let high = _arr.length - 1;
+  let mid;
+
+  while (low <= high) {
+    mid = Math.floor((low + high) / 2);
+
+    if (_arr[mid] === _tg) {
+      return mid;
+    } else if (_arr[mid] > _tg) {
+      high = mid - 1;
+    } else {
+      low = mid + 1;
+    }
+  }
+
+  return -1;
+};
+
+const binarySearch = (n, times) => {
+  let low = 0;
+  let high = times[times.length - 1] * n;
+  let highest = high;
+
+  while (low <= high) {
+    const mid = Math.floor((low + high) / 2);
+
+    let count = 0;
+    for (const time of times) {
+      count += Math.floor(mid / time);
+      if (count >= n) {
+        highest = mid;
+      }
+    }
+
+    if (count >= n) {
+      high = mid - 1;
+    } else {
+      low = mid + 1;
+    }
+  }
+
+  return highest;
+};
+
+export const solution = (n, times) => {
+  times.sort((a, b) => a - b);
+  return binarySearch(n, times);
+};
+
 export const fail_solution = (n, times) => {
   let minute = 0;
   let remaining = [...times];
@@ -9,7 +60,7 @@ export const fail_solution = (n, times) => {
     let minIdx = [0];
     let min = remaining[0];
 
-    for (let i = 1; i< timesLen; ++i) {
+    for (let i = 1; i < timesLen; ++i) {
       if (min === remaining[i]) {
         minIdx.push(i);
       }
