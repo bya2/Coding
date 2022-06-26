4b2f2b0121b2124f9aaf2fb044260dc8531be38d
bya2 <byaa1972@gmail.com>
2022-06-26 23:55:20 +0900

    Graph class 일부 수정 및 의사 코드 분리

--- src/자료구조/Graph.mjs
+++ src/자료구조/Graph.mjs
@@ -1,29 +1,73 @@
-class List {
-  _items;
-  constructor() {
-    this._items = [];
+import { Queue } from "./List.mjs";
+
+const pseudocodeOfBFS = (_root) => {
+  let queue = new Queue();
+  _root.visited = true;
+  queue.enqueue(_root);
+
+  while (!queue.isEmpty) {
+    let node = queue.dequeue();
+
+    // processing...
+
+    for (let adjNode of node.adjacent) {
+      if (!adjNode.visited) {
+        adjNode.visited = true;
+        queue.enqueue(adjNode);
+      }
+    }
   }
+};
+
+const pseudocodeOfDFS = (_root) => {
+  if (_root === null) return;
+
+  // processing...
+
+  _root.visited = true;
+
+  for (let adjNode of _root.adjacent) {
+    if (!adjNode.visited) {
+      pseudocodeOfDFS(adjNode);
+    }
+  }
+};
+
+export class Graph {
+  _root;
+
+  constructor(_root) {
+    this._root = _root;
+  }
+
+  BFS() {
+    let queue = new Queue();
+    this._root.visited = true;
+    queue.enqueue(this._root);
+
+    while (!queue.isEmpty) {
+      let node = queue.dequeue();
+
+      this.visit(node);
+
+      for (let adjNode of node.adjacent) {
+        if (!adjNode.visited) {
+          adjNode.visited = true;
+          queue.enqueue(adjNode);
+        }
+      }
+    }
+  }
+
+  DFS() {}
+
+  visit(_node) {}
 }
 
-class Queue extends LinkedList {
-  enqueue(_item) {
-    this._items.push(_item);
-  }
-  dequeue() {
-    return this._items.shift();
-  }
-}
+export class Tree extends Graph {
+  inOrder() {}
 
-class Stack extends LinkedList {
-  get peek() {
-    return this._items[this._items.length - 1];
-  }
-  push(_item) {
-    this._items.push(_item);
-  }
-  pop() {
-    return this._items.pop();
-  }
-}
+  preOrder() {}
 
-console.log()
+  postOrder() {}
+}
