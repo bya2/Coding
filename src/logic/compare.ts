d0ae5a22e3df7e94ccd1ce7e7b0b4080da6b0b17
bya2 <byaa1972@gmail.com>
2022-08-05 23:41:06 +0900

    자료구조 및 로직 코드 분리

--- src/logic/compare.ts
+++
@@ -1,3 +0,0 @@
-export const getBigger = <E extends number>(el1: E, el2: E): E => (el1 < el2 ? el2 : el1);
-
-export const getSmaller = <E extends number>(el1: E, el2: E): E => (el1 < el2 ? el1 : el2);
