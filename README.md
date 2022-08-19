ec0e16de68a8538c762a72be856f3a0dba76ccc6
bya2 <byaa1972@gmail.com>
2022-08-19 19:25:21 +0900

    fail: 프로그래머스/두 큐 합 같게 만들기 - DP, 90/100

--- README.md
+++ README.md
@@ -76,6 +76,7 @@ BFS는 최단 거리
 - [두 개 뽑아서 더하기](./src/programmers/%EB%91%90%20%EA%B0%9C%20%EB%BD%91%EC%95%84%EC%84%9C%20%EB%8D%94%ED%95%98%EA%B8%B0.mjs)
 - [조이스틱](./src/programmers/%EC%A1%B0%EC%9D%B4%EC%8A%A4%ED%8B%B1.mjs)
 - [게임 맵 최단거리](src/programmers/%EA%B2%8C%EC%9E%84%20%EB%A7%B5%20%EC%B5%9C%EB%8B%A8%EA%B1%B0%EB%A6%AC.mjs)
+- [두 큐 합 같게 만들기](src/programmers/%EB%91%90%20%ED%81%90%20%ED%95%A9%20%EA%B0%99%EA%B2%8C%20%EB%A7%8C%EB%93%A4%EA%B8%B0.mjs)
 
 ## [Hash](https://github.com/bya2/coding/blob/main/src/Hash/index.mjs)
 
--- src/programmers/두 큐 합 같게 만들기.mjs
+++ src/programmers/두 큐 합 같게 만들기.mjs
@@ -1,33 +1,36 @@
-export const solution = (q1, q2) => {
+const sum = (...args) => {
+  return args.reduce((a, b) => a + b, 0);
+};
+
+export const solution = (queue1, queue2) => {
   // 하나의 큐 POP
   // POP 요소를 다른 큐에 INSERT
   // 각 큐의 원소들의 합이 같게함. 필요 작업의 최소 횟수(POP + INSERT 1회)
 
-  const N = q1.length;
-
-  let sum1 = q1.reduce((a, b) => a + b, 0);
-  let sum2 = q2.reduce((a, b) => a + b, 0);
+  let sum1 = sum(...queue1);
+  let sum2 = sum(...queue2);
   let count = 0;
 
-  while (sum1 !== sum2) {
-    if (count > N) {
-      return -1;
+  const TOTAL = sum1 + sum2;
+  if (TOTAL % 2 !== 0) return -1;
+  const GOAL = TOTAL / 2;
+
+  while (queue1.length && queue2.length) {
+    if (sum1 === GOAL) {
+      return count;
+    } else {
+      if (sum1 < GOAL) {
+        let tmp = queue2.shift();
+        sum1 += tmp;
+        queue1.push(tmp);
+      } else {
+        sum1 -= queue1.shift();
+      }
+      count++;
     }
-
-    let p1 = q1.shift();
-    let p2 = q2.shift();
-
-    q1.push(p2);
-    q2.push(p1);
-
-    sum1 += p2 - p1;
-    sum2 += p1 - p2;
-    count++;
-
-    console.log(sum1, sum2, count, q1, q2);
   }
 
-  return count;
+  return -1;
 };
 
 export const examples__arr = [
