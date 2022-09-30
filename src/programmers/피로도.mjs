68b4c5392c224d2395cf89d813eb1277535b1e20
bya2 <byaa1972@gmail.com>
2022-09-30 13:19:14 +0900

    re: 프로그래머스/피로도 - 완전탐색(DFS) - 클래스, 콜백 함수

--- src/programmers/피로도.mjs
+++ src/programmers/피로도.mjs
@@ -1,4 +1,113 @@
+class Node {
+  data;
+  adjacencyList = new Set();
+  _marked = false;
+
+  constructor(data, adjacencyList = new Set()) {
+    this.data = data;
+    this.adjacencyList = adjacencyList;
+  }
+
+  isMarked() {
+    return this._marked;
+  }
+
+  mark() {
+    this._marked = true;
+  }
+
+  clear() {
+    this._marked = false;
+  }
+}
+
+class AdjacencyListGraph extends Map {
+  root;
+
+  constructor(node) {
+    super();
+    if (typeof node !== "undefined") {
+      this.root = node;
+    }
+  }
+
+  addVertex(node) {
+    if (!this.has(node)) {
+      this.set(node, new Set());
+    }
+  }
+
+  connectEdge(node1, node2) {
+    this.addVertex(node1);
+    this.addVertex(node2);
+
+    this.get(node1).add(node2);
+    node1.adjacencyList.add(node2);
+
+    this.get(node2).add(node1);
+    node2.adjacencyList.add(node1);
+  }
+
+  connectAll() {
+    for (let curr of this.keys()) {
+      for (let node of this.keys()) {
+        if (!Object.is(curr, node)) {
+          this.connectEdge(curr, node);
+        }
+      }
+    }
+  }
+
+  searchDepthFirst(cb, ...args) {
+    let height = 0;
+
+    const DFS = (curr, { depth }, args) => {
+      if (typeof curr === "undefined" || curr === null) throw new Error();
+
+      height = Math.max(height, depth);
+
+      if (this.has(curr)) {
+        curr.mark();
+        for (const adj of this.get(curr)) {
+          if (!adj.isMarked()) {
+            const [condition, ...args2] = cb(adj.data, ...args);
+            condition && DFS(adj, { depth: depth + 1 }, args2);
+          }
+        }
+        curr.clear();
+      }
+    };
+
+    for (const node of this.keys()) {
+      const [condition, ...args2] = cb(node.data, ...args);
+      condition && DFS(node, { depth: 0 }, args2);
+
+    }
+
+    height++;
+
+    return {
+      height,
+    };
+  }
+}
+
 export const solution = (k, dungeons) => {
+  const graph = new AdjacencyListGraph();
+  for (let i = 0, len = dungeons.length; i < len; ++i) {
+    const node = new Node(dungeons[i]);
+    graph.addVertex(node);
+  }
+  graph.connectAll();
+
+  const obj = graph.searchDepthFirst(([minimum, cost], k) => {
+    return [k >= minimum, k - cost];
+  }, k);
+
+  return obj.height;
+};
+
+export const ssolution = (k, dungeons) => {
   let height = 0;
   const len = dungeons.length;
   const visited = Array.from({ length: len }, () => false);
@@ -8,6 +117,7 @@ export const solution = (k, dungeons) => {
     visited[node] = true;
     for (let i = 0; i < len; ++i) {
       const [minimum, cost] = dungeons[i];
+      console.log(minimum, cost, k - cost);
       if (!visited[i] && k >= minimum) {
         dfs(i, k - cost, depth + 1);
       }
