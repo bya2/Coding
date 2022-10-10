6775a64175e861f3c8d25bb54002e73d09ee133e
bya2 <byaa1972@gmail.com>
2022-10-10 11:56:28 +0900

    success: 프로그래머스/삼각 달팽이(2) - 증감

---
+++ src/programmers/Lv2/삼각 달팽이.mjs
@@ -0,0 +1,65 @@
+export function solution(n) {
+  const arr = Array.from({ length: n }, (_, i) => Array(i + 1).fill(0));
+
+  for (let [num, row, col, val] = [n, -1, 0, 0]; num > 0; num -= 3) {
+    for (let i = 0; i < num; ++i) {
+      arr[++row][col] = ++val;
+    }
+
+    for (let [i, len] = [0, num - 1]; i < len; ++i) {
+      arr[row][++col] = ++val;
+    }
+
+    for (let [i, len] = [0, num - 2]; i < len; ++i) {
+      arr[--row][--col] = ++val;
+    }
+  }
+
+  return [].concat(...arr);
+}
+
+// 이게 더 빠름. 왜지?
+export function solution2(n) {
+  const arr = Array.from({ length: n }, (_, i) => Array(i + 1).fill(0));
+  let [row, col, val] = [-1, 0, 0];
+
+  while (n > 0) {
+    for (let i = 0; i < n; ++i) {
+      ++row;
+      ++val;
+      arr[row][col] = val;
+    }
+
+    for (let [i, len] = [0, n - 1]; i < len; ++i) {
+      ++col;
+      ++val;
+      arr[row][col] = val;
+    }
+
+    for (let [i, len] = [0, n - 2]; i < len; ++i) {
+      --row;
+      --col;
+      ++val;
+      arr[row][col] = val;
+    }
+
+    n -= 3;
+  }
+
+  return [].concat(...arr);
+}
+
+export const examples__arr = [
+  {
+    n: 4,
+    answer: [1, 2, 9, 3, 10, 8, 4, 5, 6, 7],
+  },
+  {
+    n: 5,
+    answer: [1, 2, 12, 3, 13, 11, 4, 14, 15, 10, 5, 6, 7, 8, 9],
+  },
+  {
+    n: 6,
+    answer: [1, 2, 15, 3, 16, 14, 4, 17, 21, 13, 5, 18, 19, 20, 12, 6, 7, 8, 9, 10, 11],
+  },
+];
--- src/run.mjs
+++ src/run.mjs
@@ -1,5 +1,5 @@
 import runner from "./runner.mjs";
-import { solution, examples__arr } from "./programmers/lv2/쿼드압축 후 개수 세기.mjs";
+import { solution, examples__arr } from "./programmers/lv2/삼각 달팽이.mjs";
 
 (function () {
   console.log("Solve:");
