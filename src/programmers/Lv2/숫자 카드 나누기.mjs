50e68b15857c8abb2b36bfef137300a87686f37d
bya2 <byaa1972@gmail.com>
2022-11-24 20:31:43 +0900

    success: 프로그래머스/숫자 카드 나누기(2) - GCD, Divisors (에러 원인: 스프레드 매개변수)

--- src/programmers/Lv2/숫자 카드 나누기.mjs
+++ src/programmers/Lv2/숫자 카드 나누기.mjs
@@ -1,26 +1,35 @@
 const N = class {
   static gcd(number1, number2) {
     return number1 % number2 === 0
-      ? number2
+      ? number2 % 1_000_000
       : this.gcd(number2, number1 % number2);
   }
 
-  static gcdBetween(...numbers) {
-    if (numbers.length === 1) return numbers[0];
+  static gcdBetween(numbers) {
     return numbers.reduce((n1, n2) => this.gcd(n1, n2));
   }
 
   static divisors(number) {
     const arr = [];
-    for (let i = 1, len = ~~(number / 2); i <= len; ++i) {
+    for (let i = 1, len = number / 2; i <= len; ++i)
       number % i === 0 && arr.push(i);
-    }
     arr.push(number);
     return arr;
   }
 };
 
+export const solution = (a, b) => {
+  const gcd1 = N.gcdBetween(a);
+  const gcd2 = N.gcdBetween(b);
+
+  let tmp1 = b.some((n) => n % gcd1 === 0) ? 0 : gcd1;
+  let tmp2 = a.some((n) => n % gcd2 === 0) ? 0 : gcd2;
+
+  return Math.max(tmp1, tmp2);
+};
+
 /**
+ * RUNTIME ERROR
  * A가 가진 카드들에 적힌 모든 숫자를 나눌 수 있고, B가 가진 카드들에 적힌 모든 숫자들 중 하나도 나눌 수 없는 양의 정수 x
  * B가 가진 카드들에 적힌 모든 숫자를 나눌 수 있고, A가 가진 카드들에 적힌 모든 숫자들 중 하나도 나눌 수 없는 양의 정수 x
  * 들 중 가장 큰 값
@@ -28,24 +37,21 @@ const N = class {
  * @param {number[]} b
  * @return {number}
  */
-export const solution = (a, b) => {
+export const solution2 = (a, b) => {
   // 1. 최대 공약수
   // - O -> RETURN
   // - X -> 최대 공약수의 약수 리스트, LOOP
   //
   // 2. 각 약수 리스트에서 큰 수를 번갈아가며 조건에 부합하는지 체크
 
-  const gcd1 = N.gcdBetween(...a);
-  const gcd2 = N.gcdBetween(...b);
+  const gcd1 = N.gcdBetween(a);
+  const gcd2 = N.gcdBetween(b);
 
   const divisors1 = N.divisors(gcd1);
   const divisors2 = N.divisors(gcd2);
 
-  const len1 = divisors1.length;
-  const len2 = divisors2.length;
-
-  let i = len1 - 1;
-  let j = len2 - 1;
+  let i = divisors1.length - 1;
+  let j = divisors2.length - 1;
 
   while (i >= 0 && j >= 0) {
     const d1 = divisors1[i];
