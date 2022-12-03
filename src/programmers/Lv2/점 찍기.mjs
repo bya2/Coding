ed45946e297145529b48c374f25e37d084ebe6b3
bya2 <byaa1972@gmail.com>
2022-12-03 13:50:20 +0900

    fail: 프로그래머스/점 찍기(2) - 시간 초과

---
+++ src/programmers/Lv2/점 찍기.mjs
@@ -0,0 +1,50 @@
+/**
+ * @param {number} k 차수
+ * @param {number} d 최대 거리
+ */
+export const solution = (k, d) => {
+  let count = 0;
+  let dd = d ** 2;
+
+  function combine() {
+    const combinations = [];
+    const visited = this.reduce((obj, t) => ((obj[t] = false), obj), {});
+
+    const searchDepthFirst = (acc = [], dict) => {
+      if (acc.length === 2) {
+        if (acc[0] ** 2 + acc[1] ** 2 <= dd) count++;
+        return;
+      }
+
+      for (const char of this) {
+        if (!dict[char]) {
+          searchDepthFirst([...acc, char], { ...dict, [char]: true });
+        }
+      }
+    };
+
+    searchDepthFirst([], { ...visited });
+    return combinations;
+  }
+
+  const list = [];
+  for (let i = 0; i <= d; i += k) {
+    list.push(i);
+    if (i ** 2 * 2 <= dd) count++;
+  }
+  combine.call(list);
+  return count;
+};
+
+export const examples__arr = [
+  {
+    k: 2,
+    d: 4,
+    answer: 6,
+  },
+  {
+    k: 1,
+    d: 5,
+    answer: 26,
+  },
+];
--- src/run.mjs
+++ src/run.mjs
@@ -1,5 +1,5 @@
 import runner from "./runner.mjs";
-import { solution, examples__arr } from "./programmers/Lv1/문자열 나누기.mjs";
+import { solution, examples__arr } from "./programmers/Lv2/점 찍기.mjs";
 
 (function () {
   console.log("Solve:");
