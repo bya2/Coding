d8fecd23a66ea2b4e32d5ca7e4264039fc10393c
bya2 <byaa1972@gmail.com>
2023-01-13 19:28:33 +0900

    Add linux runner for multi line

---
+++ src/baekjoon/linux-runner.mjs
@@ -0,0 +1,22 @@
+const solution = (inputs) => {};
+
+/**
+ * 여러 줄 입력
+ */
+(() => {
+  import("readline").then((readline) => {
+    const rl = readline.createInterface({
+      input: process.stdin,
+      output: process.stdout,
+    });
+
+    const inputs = [];
+
+    rl.on("line", (line) => {
+      inputs.push(line.toString());
+    }).on("close", () => {
+      console.log(solution(inputs));
+      process.exit();
+    });
+  });
+})();
