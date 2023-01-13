7e36120573e39943ec57c31e127aaac4859c773b
bya2 <byaa1972@gmail.com>
2023-01-13 19:27:49 +0900

    Modify windows platfrom runner and arguments

--- src/baekjoon/run.mjs
+++ src/baekjoon/run.mjs
@@ -1,7 +1,7 @@
 import runner from "./runner.mjs";
-import { solution } from "./문제집1/MooTube (Silver).mjs";
+import { solution, examples } from "./정렬/대표값2.mjs";
 
 (function () {
   console.log("Solve:");
-  runner(solution);
+  runner(solution, examples);
 })();
--- src/baekjoon/runner.mjs
+++ src/baekjoon/runner.mjs
@@ -1,10 +1,10 @@
-import * as fs from "fs";
 import * as path from "path";
+import { join } from "path";
 import * as url from "url";
 
 const __dirname = path.dirname(url.fileURLToPath(import.meta.url));
 
-class Path {
+export class Path {
   /**
    * return path from file
    * @param  {...string} paths
@@ -15,10 +15,22 @@ class Path {
   }
 }
 
-const runner = (solution) => {
-  const INPUT_DATA = process.platform === "linux" ? "/dev/stdin" : Path.fromFile("input.txt");
-  const inputs = fs.readFileSync(INPUT_DATA).toString().trim().split("\r\n");
-  solution(inputs);
+/**
+ * @param {Function} solution
+ * @param {string[]} examples
+ */
+const runner = (solution, examples) => {
+  examples.forEach((example, index) => {
+    let { inputs, answer } = example;
+    inputs = inputs.split("\n").map((s) => s.trim());
+    answer = answer
+      .split("\n")
+      .map((s) => s.trim())
+      .join("\n");
+    const result = solution(inputs);
+    console.log(`#${index + 1} ${result === answer ? "SUCCESS" : "FAIL"}`);
+    console.log(`---\n${result}\n---\n${answer}\n---`);
+  });
 };
 
 export default runner;
