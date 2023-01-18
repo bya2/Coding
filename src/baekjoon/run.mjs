9e4e8b29b75007df5f0b94447cfa174733a1f4f2
bya2 <byaa1972@gmail.com>
2023-01-18 15:26:47 +0900

    success: 백준/재귀/별 찍기 - 10 (분할정복)

--- src/baekjoon/run.mjs
+++ src/baekjoon/run.mjs
@@ -1,5 +1,5 @@
 import runner from "./runner.mjs";
-import { solution, examples } from "./재귀/알고리즘 수업 - 병합 정렬 1.mjs";
+import { solution, examples } from "./재귀/별 찍기 - 10.mjs";
 
 (function () {
   console.log("Solve:");
