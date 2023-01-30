15fcee8dcce125f5c0f544d0423e681cfd0eedb7
bya2 <byaa1972@gmail.com>
2023-01-30 15:50:16 +0900

    fail: 백준/정수론 및 조합론/이항 계수 2

--- src/baekjoon/run.mjs
+++ src/baekjoon/run.mjs
@@ -1,5 +1,5 @@
 import runner from "./runner.mjs";
-import { solution, examples } from "./정수론 및 조합론/이항 계수 1.mjs";
+import { solution, examples } from "./정수론 및 조합론/이항 계수 2.mjs";
 
 (function () {
   console.log("Solve:");
---
+++ src/baekjoon/정수론 및 조합론/이항 계수 2.mjs
@@ -0,0 +1,30 @@
+/**
+ * @param {string[]} inputs
+ */
+export const solution = (inputs) => {
+  const [n, k] = inputs[0].split(" ").map(Number);
+
+  let tri = [];
+
+  for (let i = 0; i <= n; i++) {
+    tri.push([]);
+
+    for (let j = 0; j <= i; j++) {
+      if (j === 0 || j === i) {
+        tri[i].push(1);
+      } else {
+        tri[i].push(
+          (tri[i - 1][j - 1] + tri[i - 1][j]) % 10_007
+        );
+      }
+    }
+  }
+  return tri[n][k] + "";
+};
+
+export const examples = [
+  {
+    inputs: `5 2`,
+    answer: `10`,
+  },
+];
