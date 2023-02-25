3ef2b3fb93c842c9b2a480dc08940ce9abba8545
bya2 <byaa1972@gmail.com>
2023-02-25 22:50:54 +0900

    success: 백준/백트래킹/N과 M(1) - Combine

---
+++ src/baekjoon/백트래킹/N과 M(1).mjs
@@ -0,0 +1,69 @@
+function combine(length) {
+  const combinations = [];
+
+  const recur = (acc, marked) => {
+    if (acc.length === length) {
+      combinations.push(acc.join(" "));
+      return;
+    }
+
+    for (const char of this) {
+      if (!marked[char]) recur([...acc, char], { ...marked, [char]: true });
+    }
+  };
+
+  recur(
+    [],
+    this.reduce((obj, t) => ((obj[t] = false), obj), {})
+  );
+
+  return combinations;
+}
+
+Object.defineProperty(Array.prototype, "combine", {
+  value: combine,
+});
+
+export const solution = (inputs = [""]) => {
+  const [n, m] = inputs[0].split(" ").map(Number);
+  const arr = Array.from({ length: n + 1 }, (_, i) => i);
+  arr.shift();
+  console.log(arr);
+  return arr.combine(m).join("\n");
+};
+
+export const examples = [
+  {
+    inputs: `3 1`,
+    answer: `1
+    2
+    3`,
+  },
+  {
+    inputs: `4 4`,
+    answer: `1 2 3 4
+    1 2 4 3
+    1 3 2 4
+    1 3 4 2
+    1 4 2 3
+    1 4 3 2
+    2 1 3 4
+    2 1 4 3
+    2 3 1 4
+    2 3 4 1
+    2 4 1 3
+    2 4 3 1
+    3 1 2 4
+    3 1 4 2
+    3 2 1 4
+    3 2 4 1
+    3 4 1 2
+    3 4 2 1
+    4 1 2 3
+    4 1 3 2
+    4 2 1 3
+    4 2 3 1
+    4 3 1 2
+    4 3 2 1`,
+  },
+];
