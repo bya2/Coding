daa8aad304fa7f9e35ed8609078aa6c23a0f90d5
bya2 <byaa1972@gmail.com>
2023-07-15 23:54:41 +0900

    problem 1 and logic 1

---
+++ src/해시/examples.js
@@ -0,0 +1,23 @@
+import { dict } from "./logic";
+
+const solution = (P, C) => {};
+
+const examples = {
+  "완주하지 못한 선수": [
+    {
+      participants: ["leo", "kiki", "eden"],
+      completion: ["eden", "kiki"],
+      result: "leo",
+    },
+    {
+      participants: ["marina", "josipa", "nikola", "vinko", "filipa"],
+      completion: ["josipa", "filipa", "marina", "nikola"],
+      result: "vinko",
+    },
+    {
+      participants: ["mislav", "stanko", "mislav", "ana"],
+      completion: ["stanko", "ana", "mislav"],
+      result: "mislav",
+    },
+  ],
+};
