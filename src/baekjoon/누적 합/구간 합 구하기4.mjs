324416557b32b3ff91c45daa2a878df0876ebfd7
bya2 <byaa1972@gmail.com>
2023-10-09 23:00:45 +0900

    re: 백준/누적 합/구간 합 구하기 4

--- src/baekjoon/누적 합/구간 합 구하기4.mjs
+++ src/baekjoon/누적 합/구간 합 구하기4.mjs
@@ -1,4 +1,21 @@
-export const solution = (inputs = [""]) => {
+/**
+ * @param {string[]} lines
+ */
+export const solution = (lines) => {
+  const [n, m] = lines.shift().map(Number);
+  const numbers = lines.shift().split(" ").map(Number);
+  const memo = [0];
+  for (let i = 0; i < numbers.length; ++i) memo.push(memo[i] + numbers[i]);
+  return lines
+    .map((line) => {
+      const [i, j] = line.split(" ").map((c) => +c - 1);
+      if (i === 0) return memo[j];
+      return memo[j] - memo[i - 1];
+    })
+    .join("\n");
+};
+
+export const other = (inputs = [""]) => {
   inputs.shift();
   const nums = inputs.shift().split(" ").map(Number);
   const accs = [0];
