a8497ad74402378cfadf67fe2883cf6244bda73d
bya2 <byaa1972@gmail.com>
2023-10-12 22:02:44 +0900

    watch: 백준/누적 합/나머지 합 - ?:조합식(nCr)

--- src/baekjoon/누적 합/나머지 합.mjs
+++ src/baekjoon/누적 합/나머지 합.mjs
@@ -1,20 +1,47 @@
+// ※ 조합식
+// n개중에 r개를 뽑는 조합
+// nCr = nPr/r! = n!/(r!(n-r)!)
+
 /**
  * @param {string[]} lines
  */
 export const solution = (lines) => {
-  const m = +lines[0].split(" ")[1];
-  const arr = lines[1].split(" ").map(Number);
+  const M = +lines[0].split(" ")[1];
+  const nums = lines[1].split(" ").map(Number);
 
-  const accBy = [0];
-  for (let i = 0; i < arr.length; ++i) accBy.push(accBy[i] + arr[i]);
+  const accUpTo = [0];
+  const countOfMod = new Array(M).fill(0);
+  let count = 0;
+  for (let i = 0; i < nums.length; ++i) {
+    const r = (accUpTo[i] + nums[i]) % M;
+    accUpTo.push(r);
+    countOfMod[r]++;
+    if (r === 0) count++;
+  }
+
+  console.log(count, countOfMod);
+
+  for (let i = 0; i < M; ++i) {
+    count += (countOfMod[i] * (countOfMod[i] - 1)) / 2;
+  }
+
+  return count + "";
+};
+
+export const fail = (lines) => {
+  const M = +lines[0].split(" ")[1];
+  const nums = lines[1].split(" ").map(Number);
+
+  const accUpTo = [0];
+  for (let i = 0; i < nums.length; ++i) accUpTo.push(accUpTo[i] + nums[i]);
 
   let count = 0;
-  for (let i = 1; i <= arr.length; ++i) {
-    if (accBy[i] < m) continue;
+  for (let i = 1; i <= nums.length; ++i) {
+    if (accUpTo[i] < M) continue;
     for (let j = 0; j <= i; ++j) {
-      const sum = accBy[i] - accBy[j];
-      if (sum < m) break;
-      if (sum % m === 0) ++count;
+      const sum = accUpTo[i] - accUpTo[j];
+      if (sum < M) break;
+      if (sum % M === 0) ++count;
     }
   }
 
