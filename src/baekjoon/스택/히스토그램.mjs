0de4aeb93e0252416c4d15b8eb29ec48eed45a9e
bya2 <byaa1972@gmail.com>
2023-10-23 23:34:18 +0900

    fail: 백준/스택/히스토그램

---
+++ src/baekjoon/스택/히스토그램.mjs
@@ -0,0 +1,36 @@
+/**
+ * @param {string[]} lines
+ */
+export const solution = (lines) => {
+  const [_, inputs] = lines.map(Number);
+  let tmp = inputs[0];
+
+  const recur = (arr, li, ri) => {
+    if (li === ri) return arr[li];
+
+    const mi = (li + ri) >> 1;
+    const highH = Math.max(recur(arr, li, mi), recur(arr, mi + 1, ri));
+
+    let mli = mi;
+    let mri = mi + 1;
+    const area = Math.max(highH, Math.min(arr[mli], arr[mri]));
+
+    while (mli > li || mri < ri) {
+      
+    }
+  };
+};
+
+export const examples = [
+  {
+    inputs: `7
+    2
+    1
+    4
+    5
+    1
+    3
+    3`,
+    answer: `8`,
+  },
+];
