9b3ab49e140bb01b784ce580d66688f883603107
bya2 <byaa1972@gmail.com>
2023-11-03 23:52:22 +0900

    success: 백준/투포인터/두 용액

---
+++ src/baekjoon/투포인터/두 용액.mjs
@@ -0,0 +1,41 @@
+/**
+ * @param {string[]} lines
+ */
+export const solution = (lines) => {
+  let n = +lines[0];
+  let inputs = lines[1]
+    .split(" ")
+    .map(Number)
+    .sort((a, b) => a - b);
+
+  let i = 0;
+  let j = n - 1;
+  let sum = 0;
+  let maximum = Number.MAX_SAFE_INTEGER;
+  let ans_pair = new Array(2).fill(0);
+  while (i !== j) {
+    sum = inputs[i] + inputs[j];
+    if (Math.abs(sum) < maximum) {
+      maximum = Math.abs(sum);
+      ans_pair[0] = inputs[i];
+      ans_pair[1] = inputs[j];
+    }
+    if (sum === 0) {
+      break;
+    } else if (sum > 0) {
+      j--;
+    } else if (sum < 0) {
+      i++;
+    }
+  }
+
+  return ans_pair.join(" ");
+};
+
+export const examples = [
+  {
+    inputs: `5
+    -2 4 -99 -1 98`,
+    answer: `-99 98`,
+  },
+];
